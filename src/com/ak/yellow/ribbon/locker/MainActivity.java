275dfc2a7a73af10dc86d4f0a604d6f2008b8bf2
KunHwan Ahn <kunhwon@gmail.com>
2015-07-20 12:16:39 +0900

    Remove a appcompact_v7 dependency

--- src/com/ak/yellow/ribbon/locker/MainActivity.java
+++ src/com/ak/yellow/ribbon/locker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ak.yellow.ribbon.locker;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -25,6 +28,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setFullscreenMode(Build.VERSION.SDK_INT);
         // Set up our Lockscreen
         getWindow().addFlags(
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
@@ -113,5 +117,23 @@ public class MainActivity extends Activity {
         public void onClick(View v) {
         }
 
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void setFullscreenMode(int versionCode) {
+        if (versionCode < Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            // Remember that you should never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            ActionBar actionBar = getActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
+        }
     }
 }
